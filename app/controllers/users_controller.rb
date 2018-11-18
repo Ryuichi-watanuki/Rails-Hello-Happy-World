b17858e88932f8acd9cdadd816d2b717eb35228c
Nukioman <nukky874@icloud.com>
2018-11-19 01:52:50 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Hello App!"
+      redirect_to @user # redirect_to user_url(@user) と等価
+    else
+      render 'new'
+    end
   end
   
+  private
+
+    # 7.3.2 https://railstutorial.jp/chapters/sign_up?version=5.1#sec-strong_parameters
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,12 @@ module ApplicationHelper
       page_title + " | " + base_title                 # 文字列の結合
     end
   end
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
   
 end
